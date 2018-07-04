ccda0fa8f2fd68394cf442f667633038a1ae0db0
Raminta Stebulytė <papugiuke@gmail.com>
2018-07-04 13:59:10 +0300

    Tetris idea.

---
+++ Assets/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    // Groups
+    public GameObject[] groups;
+    public void spawnNext()
+    {
+        int i = Random.Range(0, groups.Length);
+
+        Instantiate(groups[i],
+            transform.position,
+            Quaternion.identity);
+    }
+    void Start()
+    {
+        spawnNext();
+    }
+}
